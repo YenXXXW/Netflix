56d49f1555017dc5338f3012a8293f3c36f45dc1
Yen <YenXXXW@gmail.com>
2022-10-15 17:25:47 +0630

    added the head section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Netflix</title>
+        <meta name='description' content='Netflix clone by Wai'/>
+        <link rel='icon' href='/netflix-logo.png' />
+      </Head> 
+      <Component {...pageProps} />
+
+    </>
+  
+  )
 }
 
 export default MyApp
